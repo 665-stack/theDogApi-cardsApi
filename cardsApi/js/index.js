2346603bb04b39954e9b49cd1f2424e40ac8090c
unknown <mahmodulshahin665@gmail.com>
2022-05-17 13:28:14 +0600

    get input value

---
+++ cardsApi/js/index.js
@@ -0,0 +1,11 @@
+/* 
+==== How to do this api for loading from tap to search btn =====
+ 1. button event handler setup
+ 2. get input value
+
+*/
+const searchButton = () => {
+    const input = document.getElementById('input-value');
+    const inputValue = input.value;
+
+}
